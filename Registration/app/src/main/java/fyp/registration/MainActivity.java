6abfe9ca8395a452d61de5c5606428ac50e5f69e
Hassan Ahmed <mr.hassaan95@gmail.com>
2017-10-25 11:09:39 +0500

    That is latest version .. before adding background images

--- Registration/app/src/main/java/fyp/registration/MainActivity.java
+++ Registration/app/src/main/java/fyp/registration/MainActivity.java
@@ -1,7 +1,10 @@
 package fyp.registration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void SIGNUP(View view){
+        if (view.getId()==  R.id.SIGNUP){
+
+            EditText ed = (EditText)findViewById(R.id.username);
+            String str = ed.getText().toString();
+            Intent i = new Intent(MainActivity.this,Signup.class);
+         //   i.putExtra("username",str);
+           // i.putExtra("username",);
+            startActivity(i);
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
 }
---
+++ Registration/app/src/main/java/fyp/registration/Signup.java
@@ -0,0 +1,17 @@
+package fyp.registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Signup extends AppCompatActivity {
+ TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+
+    }
+}
