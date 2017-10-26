3f363c2b8b6f519a90d4dd03d5a2e5dbe6151ad6
Hassan Ahmed <mr.hassaan95@gmail.com>
2017-10-26 12:20:10 +0500

    That code is working fine with login and signup pages...   thats quite working code

---
+++ MA_Start/app/src/main/java/mobile_application/ma_start/MainActivity.java
@@ -0,0 +1,37 @@
+package mobile_application.ma_start;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void SIGNUP(View view){
+        if (view.getId()==  R.id.SIGNUP){
+
+
+            Intent i = new Intent(MainActivity.this,Signup.class);
+            //   i.putExtra("username",str);
+            // i.putExtra("username",);
+            startActivity(i);
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ MA_Start/app/src/main/java/mobile_application/ma_start/Signup.java
@@ -0,0 +1,13 @@
+package mobile_application.ma_start;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
